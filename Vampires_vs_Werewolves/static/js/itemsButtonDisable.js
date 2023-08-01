68cb56a1eff60930427fe781ee64f988593c727c
ddimitrovv <dimitrov.mitko.md@gmail.com>
2023-08-01 14:03:01 +0300

    added required level for disabeling buttons

--- Vampires_vs_Werewolves/static/js/itemsButtonDisable.js
+++ Vampires_vs_Werewolves/static/js/itemsButtonDisable.js
@@ -4,8 +4,11 @@ window.addEventListener('DOMContentLoaded', () => {
         const price = Number(element.querySelector('.item-price')
             .textContent.replace('Price: ', ''));
         const button = element.querySelector('button')
-        const heroGold = Number(button.id)
-        if (heroGold < price) {
+        const heroGold = Number(price.id)
+        const requiredLevel = Number(element.querySelector('.required-level')
+            .textContent.replace('Required level: ', ''));
+        const heroLevel = Number(element.querySelector('.required-level').id)
+        if ((heroGold < price) || (requiredLevel > heroLevel)) {
             button.style.background = '#f3efec'; // Change the color from red to gray when not enough money
             button.style.pointerEvents = 'none'; // Prevent clicking on the link when not enough money
         }
