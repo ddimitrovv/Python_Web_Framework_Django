2b32b48ab734a26593fec4373eb8b388d16c0485
ddimitrovv <dimitrov.mitko.md@gmail.com>
2023-08-17 12:14:48 +0300

    added settings for prod

--- Vampires_vs_Werewolves/Vampires_vs_Werewolves/settings.py
+++ Vampires_vs_Werewolves/Vampires_vs_Werewolves/settings.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 from celery.schedules import crontab
+import os
+from dotenv import load_dotenv
 
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,12 +14,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'secret-key'
+SECRET_KEY = os.getenv('SECRET_KEY', None)
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.getenv('DEBUG', 1)))  # Now is True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(' ')
 
 
 # Application definition
@@ -77,11 +80,11 @@ WSGI_APPLICATION = 'Vampires_vs_Werewolves.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "vampire_vs_werewolves_db",
-        "USER": "postgres-user",
-        "PASSWORD": "password",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+        "NAME": os.getenv('DB_NAME'),
+        "USER": os.getenv('DB_USER'),
+        "PASSWORD": os.getenv('DB_PASSWORD'),
+        "HOST": os.getenv('DB_HOST'),
+        "PORT": os.getenv('DB_PORT'),
     }
 }
 
@@ -141,8 +144,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'profiles.CustomUser'
 
 # Celery Configuration
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-result_backend = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL')
+result_backend = os.getenv('RESULT_BACKEND')
 CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
 
 # # Schedule the task to run initially and then repeat every day at the start of the day
